89d903a5a6627fb5e47f8fadb5fa4aaf501a9ace
Juan Tellez <jmtp12poke@gmail.com>
2023-05-31 01:29:45 -0600

    redux example addTask

---
+++ src/components/TaskForm.js
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { addTask } from "../features/tasks/taskSlice"
+import { v4 as uuid } from "uuid"
+
+const TaskForm = () => {
+
+    const [task, setTask] = useState({
+        title: '',
+        description: ''
+    })
+
+    const dispatch = useDispatch()
+
+    const handleChange = e => {
+        setTask({
+            ...task,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        dispatch(addTask(
+            {
+                ...task,
+                id: uuid()
+            }
+        ))
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="title"
+                    type="text"
+                    placeholder="title"
+                    onChange={handleChange}>
+                </input>
+                <textarea
+                    name="description"
+                    placeholder="description"
+                    onChange={handleChange}>
+                </textarea>
+                <button>Save</button>
+            </form>
+        </div>
+    )
+}
+
+export default TaskForm
